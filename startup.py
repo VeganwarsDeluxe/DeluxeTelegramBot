476c8ccadf6f536df4c21a84aaafd4a988bd4b4c
vezono <vezono.lab@gmail.com>
2024-11-12 14:48:21 -0500

    feat: pre-1.4.0 release

--- startup.py
+++ startup.py
@@ -1,11 +1,7 @@
-from mongoengine import connect
-
-from config import mongo_url, default_locale
 from VegansDeluxe.core import Engine, translator
 
+from config import default_locale
 from game.Matches.Matchmaker import Matchmaker
-
-connect(host=mongo_url, db='viro')
 
 translator.load_folder("localizations")
 translator.load_folder("game/localizations")
